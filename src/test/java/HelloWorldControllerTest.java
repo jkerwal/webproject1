4b78d76c3810f649af581b771348c9f22b41ed70
jkerwal <38856997+jkerwal@users.noreply.github.com>
2018-06-07 17:05:06 +0530

    Update HelloWorldControllerTest.java

--- src/test/java/HelloWorldControllerTest.java
+++ src/test/java/HelloWorldControllerTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -14,11 +16,11 @@ public class HelloWorldControllerTest {
 
 	
 
-	//@Test
-	public void testAppStarted() {
-
+	@Test
+	public void testAppStarted() throws IOException {
+		String content = new String(Files.readAllBytes(Paths.get("/var/lib/jenkins/ec2_ip.txt")));
 	    WebDriver driver = new HtmlUnitDriver();
-	    driver.get("http://localhost:8080/HelloWorld/");
+	    driver.get("http://"+content.trim()+":8080/HelloWorld/");
 	    // Check that the login page is shown
 	    assertEquals("Spring 4 MVC - HelloWorld Index Page", driver.getTitle());
 	}
